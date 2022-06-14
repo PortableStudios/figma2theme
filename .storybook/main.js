b6afed7dc160b23d494825a4854d3dd6af9f1a27
Darcy Glennen <darcyglennen@gmail.com>
2022-06-15 09:58:58 +1000

    chore: update `storybook` to use `webpack` v5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,14 @@
-const path = require('path');
-const toPath = (_path) => path.join(process.cwd(), _path);
-
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
+  core: {
+    builder: 'webpack5',
+  },
+  features: {
+    emotionAlias: false,
+  },
   addons: [
     'storybook-addon-designs',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
   ],
-  webpackFinal: async (config) => {
-    // Fix `framer-motion` build error
-    // https://github.com/framer/motion/issues/1307#issuecomment-966827629
-    config.module.rules.push({
-      type: 'javascript/auto',
-      test: /\.mjs$/,
-      include: /node_modules/,
-    });
-
-    // Copied from Chakra UI repo to make Chakra work correctly in Storybook
-    // https://github.com/chakra-ui/chakra-ui/blob/main/.storybook/main.js
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
-        },
-      },
-    };
-  },
 };
