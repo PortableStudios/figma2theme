7fcf7e9781e63af2e27df4c60fabbea7d8af1502
Darcy Glennen <darcyglennen@gmail.com>
2024-06-18 14:28:42 +1000

    remove prettier formatting of the generated file to fix issue with certain workspaces

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,26 +1,14 @@
 import ejs from 'ejs';
-import path from 'path';
 import fs from 'fs-extra';
-import prettier from 'prettier';
 import type { Data } from 'ejs';
 
-const prettierConfigFile = path.resolve(__dirname, '../../.prettierrc');
-
-// Run Prettier on TypeScript code using the config file
-const formatFileContents = async (contents: string) => {
-  const options = await prettier.resolveConfig(prettierConfigFile);
-
-  return prettier.format(contents, { ...options, parser: 'typescript' });
-};
-
-// Render an EJS template with the given data, format it with Prettier and write the result to the output path
+// Render an EJS template with the given data and write the result to the output path
 export const renderTemplate = async (
   templatePath: string,
   outputPath: string,
   data: Data
 ) => {
-  let contents = await ejs.renderFile(templatePath, data);
-  contents = await formatFileContents(contents);
+  const contents = await ejs.renderFile(templatePath, data);
 
   return fs.outputFile(outputPath, contents);
 };
